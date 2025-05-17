812793a2906cf85ec0fbb9931a2babe11ad3d09b
dsampsonST <dsampson@perditionsecurity.com>
2025-05-17 09:12:34 -0400

    setup scaffolding for project and added entities

---
+++ EcoSim.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace EcoSim.Core;
+
+public class Class1
+{
+
+}
---
+++ EcoSim.UI/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using EcoSim.Core;
+using EcoSim.Core.Entities;
+using System.Windows.Media;
+
+namespace EcoSim.UI
+{
+    public partial class MainWindow : Window
+    {
+        private readonly Simulation _sim;
+        private readonly DispatcherTimer _timer;
+        private const int CellSize = 10;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _sim = new Simulation(width: 50, height: 50);
+
+            _timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(200)
+            };
+            _timer.Tick += OnTick;
+            _timer.Start();
+        }
+
+        private void OnTick(object sender, EventArgs e)
+        {
+            _sim.Tick();
+            RenderEntities();
+        }
+
+        private void RenderEntities()
+        {
+            WorldCanvas.Children.Clear();
+
+            foreach (var ent in _sim.Entities)
+            {
+                var rect = new Rectangle
+                {
+                    Width  = CellSize,
+                    Height = CellSize,
+                    Fill   = ent switch
+                    {
+                        Herbivore _ => Brushes.Blue,
+                        Carnivore _ => Brushes.Red,
+                        Vegetation _ => Brushes.Green,
+                        _            => Brushes.Gray
+                    }
+                };
+
+                Canvas.SetLeft(rect, ent.X * CellSize);
+                Canvas.SetTop( rect, ent.Y * CellSize);
+                WorldCanvas.Children.Add(rect);
+            }
+        }
+    }
+}
